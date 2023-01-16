6236f8e083c0a0d0126b15338d6879610f9f37ec
Frac7 <francescacella96@gmail.com>
2023-01-16 23:50:32 +0100

    Improve background sync configuration

--- packages/frontend/src/utils/index.ts
+++ packages/frontend/src/utils/index.ts
@@ -1,19 +1,14 @@
 import { BackgroundSyncPlugin } from "workbox-background-sync";
 import { registerRoute } from "workbox-routing";
 import { HTTPMethod } from "workbox-routing/utils/constants";
-import { NetworkOnly } from "workbox-strategies";
+import { NetworkFirst } from "workbox-strategies";
 
 /**
  * A class implementing the fetchDidFail lifecycle callback. This makes it easier to add failed requests to a background sync Queue.
  * https://developer.chrome.com/docs/workbox/reference/workbox-background-sync/
  * https://developer.chrome.com/docs/workbox/modules/workbox-background-sync/
  */
-const backgroundgSyncPlugin = new BackgroundSyncPlugin("bookings", {
-  maxRetentionTime: 5, // Retry for max of 5 minutes,
-  onSync: (options) => {
-    console.log("onSync", options);
-  },
-});
+const backgroundgSyncPlugin = new BackgroundSyncPlugin("bookings");
 
 /**
  * Registers a route with the router.
@@ -28,15 +23,29 @@ const registerRouterToBackgroundSync = ({
 }) =>
   registerRoute(
     route,
-    new NetworkOnly({
+    new NetworkFirst({
       plugins: [backgroundgSyncPlugin],
     }),
     method
   );
 
 export const registerRoutes = () => {
-  registerRouterToBackgroundSync({ route: /bookings\*/, method: "GET" });
-  registerRouterToBackgroundSync({ route: /bookings\*/, method: "POST" });
-  registerRouterToBackgroundSync({ route: /bookings\*/, method: "PUT" });
-  registerRouterToBackgroundSync({ route: /bookings\*/, method: "DELETE" });
+  const escapedBaseUrl = String.raw`${process.env.REACT_APP_BASE_URL}`;
+
+  registerRouterToBackgroundSync({
+    route: new RegExp(`${escapedBaseUrl}/bookings\\?month=(\\d)+`),
+    method: "GET",
+  });
+  registerRouterToBackgroundSync({
+    route: new RegExp(`${escapedBaseUrl}/bookings/.*`),
+    method: "GET",
+  });
+  registerRouterToBackgroundSync({
+    route: new RegExp(`${escapedBaseUrl}/bookings`),
+    method: "POST",
+  });
+  registerRouterToBackgroundSync({
+    route: new RegExp(`${escapedBaseUrl}/bookings/.*`),
+    method: "DELETE",
+  });
 };
